812ec87ab5f1e2a24add112a23209843315a5e0f
Phi-star <saliuphilip8@gmail.com>
2025-01-21 03:23:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-
-    function validateForm() {
-      const form = document.getElementById('info-form');
-      if (form.checkValidity()) {
-        window.open('https://wa.me/1234567890', '_blank'); // Replace with Paola's WhatsApp number
+  function validateForm() {
+    const form = document.getElementById('info-form');
+    const phone = document.getElementById('phone');
+    
+    // Check if form is valid
+    if (form.checkValidity()) {
+      window.open('https://wa.me/48459239068',); // Replace with Paola's WhatsApp number
+    } else {
+      // Check for specific fields and provide custom messages
+      if (!phone.checkValidity()) {
+        alert('Please enter a valid phone number with 10 to 15 digits. with your country code');
       } else {
         alert('Please fill in all required details.');
       }
     }
- 
+  }
